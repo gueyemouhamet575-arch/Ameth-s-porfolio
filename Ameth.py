4bd2cf76661d01e56e861dc3eae404a127fcd8a1
gueyemouhamet575-arch <gueyemouhamet575@gmail.com>
2026-05-08 12:12:05 +0000

    Update Ameth.py

--- Ameth.py
+++ Ameth.py
@@ -1,1 +1,37 @@
+import streamlit as st
 
+
+st.title("welcome to ameth's porfolio")
+st.write("INGENIEUR ET TECHNICIEN EN SCIENCE AGRICOLE ALIMENTAIRE ET NUTRITIONNELLE")
+
+st.header("profil")
+st.write("nom: Gueye")
+st.write("prenom: Mouhamet")
+st.write("date de naissance: 12/01/2005")
+st.write("ADDRESSE: DAKAR/LIBERTE6")
+        
+st.header("parcours")
+st.write("2026-2027 licence en science agricole et alimentaire")
+st.write("2023-2024 baccalaureat srie S2")
+ 
+
+st.header("competences techniques")
+
+st.write("amenagement du sol")
+st.write("elevage de betail")
+st.write("JE m'aitrise les nouvelles methodes et techniques agricoles")
+
+st.header("Projet realises")
+        
+st.write("j'ai amenage mon propre champ d'arachide durant les vacances dans mon village natal")
+st.image("B.jpeg",width =250)
+st.write("chez moi j'ai planté pas mal d'arbre,des legumes aussi")
+st.image("C.jpeg" ,width =250)
+
+
+st.write("email:gueye.mouhamet575@gmail.com")
+st.write("addresse:liberte6extention")
+
+
+
+        
